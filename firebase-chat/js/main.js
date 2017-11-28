05b6eaaad1823a6187df7952f64f5fee1387ab18
Daniel Usani <daniel.e.usani@gmail.com>
2017-11-27 20:49:35 -0500

    Chat Screen
    Added the basic chat screen and also styled in css

--- firebase-chat/js/main.js
+++ firebase-chat/js/main.js
@@ -15,5 +15,37 @@ var messages = db.ref("messages/");
 var provider = new firebase.auth.FacebookAuthProvider();
 
 var login = () => {
-    
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(token, user);
+      
+      // ...
+      $("#login-screen").fadeOut(() => {
+        $("#chat-screen").html(`
+         
+            <div class="header">Header will be here</div>
+            <div class="messages">Messages will be here</div>
+            <div class="input">
+              <input type="text"> <span class="ion-paper-airplane"></span>
+            </div>
+          
+        `).fadeIn();
+      });
+
+    }).catch(function(error){
+      // Handle errors here
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    });
 }
+
+$( document ).ready(function() {
+  $('#login-btn').click(login);
+});
